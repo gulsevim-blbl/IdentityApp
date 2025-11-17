46f4391d9c7fd374e60c45ea9e1c99a6aa1fc4af
gulsevim-blbl <gulsevimblbl@gmail.com>
2025-11-17 20:56:46 +0300

    databaseconnection

---
+++ Models/IdentitySeedData.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models
+{
+    public class IdentitySeedData
+    {
+        //ilk başta hangi kullanıcıların eklenmesi gerektiğini belirten kodlar buraya gelecek
+         private const string adminUser = "Admin";
+        private const string adminPassword = "Admin_123";
+
+        public static async void IdentityTestUser(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>();
+
+            if(context.Database.GetAppliedMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByNameAsync(adminUser);
+
+            if(user == null)
+            {
+                user = new IdentityUser {
+                    UserName = adminUser,
+                    Email = "admin@gulsevimblbl.com",
+                    PhoneNumber = "05457777777"                    
+                };
+                //bunler haricinde ad soyad şehir cinsiyet gibi  bilgileri ıdentityuser da tutamıyoruz.IdentityUserdan ürettiğimiz ekstra bir sınıf var orada tutulur.
+
+                await userManager.CreateAsync(user, adminPassword); //şifreyi hashleyip veritabanına kaydedeceğimiz için IdentityUser ın içine eklemedik doğrudan.
+            }
+        }
+    } 
+}
